04516dd02099596f17d243630e19de7ba7b147b6
Bill Auclair <2160770+bauclair@users.noreply.github.com>
2026-07-27 17:43:54 -0700

    Enumerate wallpaper shader files.

--- src/manage_wallpaper.rs
+++ src/manage_wallpaper.rs
@@ -17,6 +17,21 @@ pub fn run(
             )?;
 
 
+    let shader_files =
+        crate::locate_wallpaper::locate_shader_files(
+            &wallpaper_directory
+        )
+        .map_err(
+            |error| {
+                format!(
+                    "Unable to enumerate wallpaper shaders in {}: {}",
+                    wallpaper_directory.display(),
+                    error,
+                )
+            }
+        )?;
+
+
     println!(
         "Screenshaver {}\n",
         env!(
@@ -50,6 +65,54 @@ pub fn run(
         "    Wallpaper directory: {}",
         wallpaper_directory.display()
     );
+
+
+    println!();
+
+
+    println!(
+        "Compatible wallpaper shaders: {}",
+        shader_files.len()
+    );
+
+
+    if shader_files.is_empty() {
+
+        println!(
+            "    No .glsl, .fs, or .shaver files were found."
+        );
+
+    } else {
+
+        for shader_file in
+            &shader_files
+        {
+            let display_name =
+                shader_file
+                    .file_name()
+                    .and_then(
+                        |name| {
+                            name.to_str()
+                        }
+                    )
+                    .unwrap_or_else(
+                        || {
+                            shader_file
+                                .as_os_str()
+                                .to_str()
+                                .unwrap_or(
+                                    "<invalid filename>"
+                                )
+                        }
+                    );
+
+
+            println!(
+                "    {}",
+                display_name
+            );
+        }
+    }
 
 
     println!();
