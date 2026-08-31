962a29f0e887f6b76afd5b094e1308fe14108947
Bill Auclair <2160770+bauclair@users.noreply.github.com>
2026-08-31 09:40:15 -0700

    Test #4 of the Fedora/GNOME lock screen.

--- src/present_screen_lock_gnome.rs
+++ src/present_screen_lock_gnome.rs
@@ -10,8 +10,8 @@ use sdl2::video::GLProfile;
 
 use crate::render_frame::FrameRenderEngine;
 
-const TRANSPORT_WIDTH: u32 = 640;
-const TRANSPORT_HEIGHT: u32 = 360;
+const TRANSPORT_WIDTH: u32 = 1280;
+const TRANSPORT_HEIGHT: u32 = 720;
 const TRANSPORT_ROWSTRIDE: u32 = TRANSPORT_WIDTH * 4;
 const TRANSPORT_FRAME_INTERVAL: Duration = Duration::from_millis(100);
 const TRANSPORT_FILENAME: &str = "screenshaver-lock-frame.shm";
