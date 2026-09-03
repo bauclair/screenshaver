219c022b3a543d94f4f518ac71c9e89ad70073f7
Bill Auclair <2160770+bauclair@users.noreply.github.com>
2026-09-03 08:35:27 -0700

    Test #20 of the Mint/Xfce lock screen.

--- src/main.rs
+++ src/main.rs
@@ -124,36 +124,6 @@ use std::time::Duration;
 
 fn main() {
 
-    if crate::present_authentication_xfce::is_helper_process() {
-
-        let logfile =
-            crate::locate_paths::runtime_log_path();
-
-
-        crate::logger::ensure_log_exists(
-            &logfile
-        );
-
-
-        if let Err(error) =
-            crate::present_authentication_xfce::run_helper(
-                &logfile
-            )
-        {
-
-            crate::logger::error(
-                &logfile,
-                &format!(
-                    "[LOCK] XFCE authentication-presentation helper failed: {}",
-                    error,
-                ),
-            );
-        }
-
-
-        return;
-    }
-
     let command =
         match crate::parse_arguments::parse() {
 
@@ -510,23 +480,6 @@ fn main() {
 
 
     if xfce_presentation_child {
-
-        if let Err(error) =
-            crate::present_authentication_xfce::launch_helper(
-                &logfile
-            )
-        {
-
-            crate::logger::warning(
-                &logfile,
-                &format!(
-                    "[LOCK] Unable to launch XFCE authentication-presentation helper; shader presentation will continue without the Screenshaver authentication widget: {}",
-                    error,
-                ),
-            );
-        }
-
-
         match crate::present_screen_lock_xfce::detect_presentation_window(
             &logfile
         ) {
